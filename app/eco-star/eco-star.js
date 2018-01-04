cec12d9383eae308fe5073ca501c35b4cb5c39a6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-01-04 15:30:15 +0100

    control 0 et 5

--- app/eco-star/eco-star.js
+++ app/eco-star/eco-star.js
@@ -4,13 +4,27 @@
     const app = angular.module('eco-star', []);
 
     app.component('ecoStar', {
-       template: 'kiki',
-       controller: class EcoStar {
-           $onInit() {
-               console.log('ecostar init');
-           }
-       }
+        bindings: {
+            note: '=',
+        },
+        controller: function EcoStarCtrl($scope, $element) {
+            $scope.$watch('$ctrl.note', () => {
+                console.log('ecostar init');
+                let note = this.note || 3;
+                note = (note > 5) ? 5 : note;
+                note = (note < 0) ? 0 : note;
+                let html = '';
+                for (let i = 0; i < note; i++) {
+                    html += '<img src="eco-star/img/yellow_star.png">';
+                }
+                for (let i = note; i < 5; i++) {
+                    html += '<img src="eco-star/img/white_star.png">';
+                }
+                html = `<div>${html}</div>`;
+                $element.html(html);
+            });
+        }
     });
 
-  
+
 })();
